f919e4dc85edb45d18059eacd38aff22eeccc7ca
Harshit Thakur <thakurharshit.dev@gmail.com>
2026-07-18 10:23:40 +0530

    test lintstaged 2

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 console.log("Server is running on port 3000");
 
 function a(x: number) {
-    console.log(x);
-    const z={
-        d:1
-    }
-    const a=z.d;
-    console.log(a);
+  console.log(x);
+  const z = {
+    d: 1,
+  };
+  const a = z.d;
+  console.log(a);
 }
 
 a(1);
