5de5bb119dd4be127d7efa8a08da14c5b5af4845
Harshit Thakur <thakurharshit.dev@gmail.com>
2026-07-19 12:31:17 +0530

    add Jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    ignores: ["dist", "node_modules", "eslint.config.mjs"],
+    ignores: ["dist", "node_modules", "eslint.config.mjs", "jest.config.cjs"],
   },
   {
     languageOptions: {
---
+++ jest.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.jest.json",
+      },
+    ],
+  },
+  testMatch: ["**/?(*.)+(spec|test).[tj]s?(x)"],
+};
