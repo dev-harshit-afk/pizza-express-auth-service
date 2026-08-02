e392212bc8edb6c3c139d5ae48fc31f878b90cea
Harshit Thakur <thakurharshit.dev@gmail.com>
2026-08-02 19:42:22 +0530

    setup coverage

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,14 @@
 module.exports = {
   testEnvironment: "node",
   verbose: true,
+  collectCoverage: true,
+  coverageProvider: "v8",
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/*.test.ts",
+    "!src/tests/**",
+    "!**/node_modules/**",
+  ],
   transform: {
     "^.+\\.(ts|tsx)$": [
       "ts-jest",
