e864b41c858b1d17762aab1a8a4aec8feca1f070
Harshit Thakur <thakurharshit.dev@gmail.com>
2026-08-09 20:12:01 +0530

    fix cors error

--- src/app.ts
+++ src/app.ts
@@ -10,9 +10,16 @@ import authRoutes from "./routes/auth";
 import cookieParser from "cookie-parser";
 import tenantRoutes from "./routes/tenant";
 import userRoutes from "./routes/user";
+import cors from "cors";
 
 const app = express();
 app.use(express.static("public", { dotfiles: "allow" }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
