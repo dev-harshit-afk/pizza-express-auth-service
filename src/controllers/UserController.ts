31788cb2c7a884c3b542be6115edfa073458b93e
Harshit Thakur <thakurharshit.dev@gmail.com>
2026-09-02 22:07:09 +0530

    add global error handler

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -14,7 +14,7 @@ export class UserController {
     try {
       const result = validationResult(req);
       if (!result.isEmpty()) {
-        return res.status(400).json({ errors: result.array() });
+        return next(createHttpError(400, result.array()[0].msg as string));
       }
 
       const { firstName, lastName, email, password, tenantId, role } = req.body;
